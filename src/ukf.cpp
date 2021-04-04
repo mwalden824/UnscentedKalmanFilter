c640d391c64e68a2cb9e2d9760c91845852bb7b2
Mr. Walden <mwalden824@gmail.com>
2021-04-04 20:10:14 +0100

    Began filling in functions.  Completed coding Prediction function.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -63,6 +63,69 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
    * TODO: Complete this function! Make sure you switch between lidar and radar
    * measurements.
    */
+
+  // If this is the first measurement, initialize state vector (x_) and
+  // covariance matrix (P_) then toggle initialized flag and return
+  if (!is_initialized_) 
+  {
+    // Is measurement RADAR or LIDAR?
+    if (meas_package.sensor_type_ == MeasurementPackage::LASER)
+    {
+      // Initialize state vector (x_)
+      x_.fill(0);
+      x_(0) = meas_package.raw_measurements_(0);
+      x_(1) = meas_package.raw_measurements_(1);
+
+      // Initialize covariance matrix (P_)
+      P_  <<  std_laspx_*std_laspx_,  0,                      0,                    0,                        0,
+              0,                      std_laspy_*std_laspy_,  0,                    0,                        0,
+              0,                      0,                      std_radr_*std_radr_,  0,                        0,
+              0,                      0,                      0,                    std_radphi_*std_radphi_,  0,
+              0,                      0,                      0,                    0,                        std_radrd_*std_radrd_;
+
+      // Set initialized flag to true
+      is_initialized_ = true;
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::RADAR)
+    {
+      // Initialize state vector (x_)
+      x_.fill(0);
+      x_(0) = meas_package.raw_measurements_(0);
+      x_(1) = meas_package.raw_measurements_(1);
+      x_(2) = meas_package.raw_measurements_(2);
+
+      // Initialize covariance matrix (P_)
+      P_  <<  1,  0,  0,  0,  0,
+              0,  1,  0,  0,  0,
+              0,  0,  1,  0,  0,
+              0,  0,  0,  1,  0,
+              0,  0,  0,  0,  1;
+
+      // Set initialized flag to true
+      is_initialized_ = true;
+    }
+    else
+    {
+      cout << "ERROR: Unknown Sensor Type." << endl;
+      return;
+    }
+  }
+  else  // Not First measurement
+  {
+    // Is measurement RADAR or LIDAR?
+    if (meas_package.sensor_type_ == MeasurementPackage::LASER)
+    {
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::RADAR)
+    {
+    }
+    else
+    {
+      cout << "ERROR: Unknown Sensor Type." << endl;
+      return;
+    }
+
+  }
 }
 
 void UKF::Prediction(double delta_t) {
@@ -71,6 +134,91 @@ void UKF::Prediction(double delta_t) {
    * Modify the state vector, x_. Predict sigma points, the state, 
    * and the state covariance matrix.
    */
+
+  // Initialize an augmented state vector, covariance matrix, 
+  // and Augmented Sigma Point Matrix
+  VectorXd x_aug = VectorXd(7);
+  MatrixXd P_aug = MatrixXd(7, 7);
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+  // Fill in augmented state vector (x_aug)
+  x_aug.head(5) = x_;
+  x_aug(5) = 0;
+  x_aug(6) = 0;
+  
+  // Fill in augmented covariance matrix (P_aug)
+  P_aug.fill(0);
+  P_aug.topLeftCorner(5, 5) = P_;
+  P_aug(5, 5) = std_a_*std_a_;
+  P_aug(6, 6) = std_yawdd_*std_yawdd_;
+
+  // Calculate the square root of the Augmented Covariance Matrix
+  MatrixXd PaugSqrt = P_aug.llt().matrixL();
+
+  // Calculate and fill in the Augmented Sigma Point Matrix
+  // i.e. Generate Sigma Points
+  Xsig_aug.col(0) = x_aug;
+  for (int i = 1; i < (n_aug_+1); i++)
+  {
+    Xsig_aug.col(i)         = x_aug + sqrt(lambda+n_aug_) * PaugSqrt.col(i-1);
+    Xsig_aug.col(i+n_aug_)  = x_aug - sqrt(lambda+n_aug_) * PaugSqrt.col(i-1);
+  }
+
+  // Calculate (1/2)t^2 for multiple use
+  float delT2 = 0.5 * delta_t * delta_t;
+
+  // For each column in the Augmented Sigma Point Matrix, calculate the predicted sigma points
+  // i.e. Predict Sigma Points
+  for (int i = 0; i < (2 * n_aug_ + 1); i++)
+  {
+    float vk = Xsig_aug(2,i);
+    float psi = Xsig_aug(3,i);
+    float psiDot = Xsig_aug(4,i);
+    float nu_a = Xsig_aug(5,i);
+    float nu_psiDot = Xsig_aug(6,i);
+
+    // Prevent division by 0  (NOTE:  THere should by some minimum value threshold here instead)
+    if (psiDot != 0)
+    {
+      Xsig_pred_(0,i) = Xsig_aug(0,i) + (vk/psiDot)*(sin(psi+psiDot*delta_t)-sin(psi)) + delT2*cos(psi)*nu_a; 
+      Xsig_pred_(1,i) = Xsig_aug(1,i) + (vk/psiDot)*(-cos(psi+psiDot*delta_t)+cos(psi)) + delT2*sin(psi)*nu_a;
+      Xsig_pred_(2,i) = Xsig_aug(2,i) + delta_t*nu_a;
+      Xsig_pred_(3,i) = Xsig_aug(3,i) + psiDot*delta_t+delT2*nu_psiDdot;
+      Xsig_pred_(4,i) = Xsig_aug(4,i) + delta_t*nu_psiDdot;
+    }
+    else  // psiDot == 0
+    {
+      Xsig_pred_(0,i) = Xsig_aug(0,i) + vk*cos(psi)*delta_t + delT2*cos(psi)*nu_a;
+      Xsig_pred_(1,i) = Xsig_aug(1,i) + vk*sin(psi)*delta_t + delT2*sin(psi)*nu_a;
+      Xsig_pred_(2,i) = Xsig_aug(2,i) + delta_t*nu_a;
+      Xsig_pred_(3,i) = Xsig_aug(3,i) + psiDot*delta_t + delT2*nu_psiDdot;
+      Xsig_pred_(4,i) = Xsig_aug(4,i) + delta_t*nu_psiDdot;
+    }
+  }
+
+  // Calculate weights
+  weights_ = VectorXd(2*n_aug_+1);
+  weights_(0) = lambda_ / (lambda_ + n_aug_);
+  for (int i = 1; i < (2*n_aug_+1); i++)
+  {
+    weights_(i) = 1/(2*(lambda_+n_aug_));
+  }
+
+  // Calculate predictied state vector
+  // i.e. Predict mean
+  x_.fill(0);
+  for (int i = 0; i < (2*n_aug_+1); i++)
+  {
+    x_ += weights_(i)*Xsig_pred_.col(i);    
+  }
+
+  // Calculate predicted covariance matrix
+  // i.e. Predict Covariance
+  P_.fill(0);
+  for (int i = 0; i < (2*n_aug_+1); i++)
+  {
+    P_ += weights_(i)*(Xsig_pred_.col(i)-x_)*(Xsig_pred_.col(i)-x_).transpose();    
+  }
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
@@ -89,4 +237,13 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
    * covariance, P_.
    * You can also calculate the radar NIS, if desired.
    */
+   MatrixXd Tc = MatrixXd(n_x_, 3);
+
+   // Normalize angle
+   while (zDiff > M_PI) zDiff -= 2 * M_PI;
+   while (zDiff < M_PI) zDiff += 2 * M_PI;
+
+   // Update state and mean covariance matrices
+   x_ = x_ + K * (z - z_pred);
+   P_ = P_ - K * S * K.transpose();
 }
